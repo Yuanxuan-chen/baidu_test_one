41485c7b6d69cc6f45730c0de4dd0b51da49d921
Yuanxuan <1045010589@qq.com>
2020-09-18 16:42:11 +0800

    测试套件添加测试用例方法二

--- runner.py
+++ runner.py
@@ -3,7 +3,15 @@ from page_test.test_search_page import TestSearchPage
 
 # 测试套件
 suite = unittest.TestSuite()
+
+# 添加测试用例
+# 方法一
 suite.addTest(TestSearchPage('test_click_logo'))
+# suite.addTests(TestSearchPage('test_search_page'))
+
+# 方法二
+# cases = [TestSearchPage('test_click_logo'), TestSearchPage('test_search_page')]
+# suite.addTests(cases)
 
 # 运行器
 runner = unittest.TextTestRunner()
