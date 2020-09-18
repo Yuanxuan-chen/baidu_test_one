2e2efd69edbfeb7ef3f86617d02b395d262e2e58
Yuanxuan <1045010589@qq.com>
2020-09-18 16:54:59 +0800

    测试套件添加测试用例方法三, 批量导入

--- runner.py
+++ runner.py
@@ -6,13 +6,17 @@ suite = unittest.TestSuite()
 
 # 添加测试用例
 # 方法一
-suite.addTest(TestSearchPage('test_click_logo'))
+# suite.addTest(TestSearchPage('test_click_logo'))
 # suite.addTests(TestSearchPage('test_search_page'))
 
 # 方法二
 # cases = [TestSearchPage('test_click_logo'), TestSearchPage('test_search_page')]
 # suite.addTests(cases)
 
+# 方法三, 批量导入测试用例
+test_dir = './'
+discover = unittest.defaultTestLoader.discover(start_dir=test_dir, pattern='test*.py')
+
 # 运行器
 runner = unittest.TextTestRunner()
-runner.run(suite)
+runner.run(discover)
