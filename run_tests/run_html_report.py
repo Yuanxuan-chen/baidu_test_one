b9aa875022adadba172454183fb3c270e7d08936
Yuanxuan <1045010589@qq.com>
2020-09-19 11:12:11 +0800

    html测试报告名称 + 时间戳

--- run_tests/run_html_report.py
+++ run_tests/run_html_report.py
@@ -1,4 +1,6 @@
 import unittest
+import time
+
 from HTMLTestRunner import HTMLTestRunner
 
 # 批量导入测试用例
@@ -10,7 +12,9 @@ suite = unittest.defaultTestLoader.discover(start_dir=test_dir, pattern='test*.p
 # runner.run(suite)
 
 # 生成html测试报告
-fp = open("../test_report/first_report.html", "wb")
+# html测试报告名称 + 时间戳
+now_time = time.strftime("%Y-%m-%d %H_%M_%S")
+fp = open("../test_report/" + now_time + "test_report.html", "wb")
 runner = HTMLTestRunner(stream=fp, title="first_report", description="试一试: first_report")
 runner.run(suite)
 fp.close()
