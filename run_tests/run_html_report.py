df1339e3742218b9bc7a92018945bf7c549547fb
Yuanxuan <1045010589@qq.com>
2020-09-19 11:03:56 +0800

    生成html测试报告

--- run_tests/run_html_report.py
+++ run_tests/run_html_report.py
@@ -6,5 +6,11 @@ test_dir = '../test_case'
 suite = unittest.defaultTestLoader.discover(start_dir=test_dir, pattern='test*.py')
 
 # 运行器
-runner = unittest.TextTestRunner()
+# runner = unittest.TextTestRunner()
+# runner.run(suite)
+
+# 生成html测试报告
+fp = open("../test_report/first_report.html", "wb")
+runner = HTMLTestRunner(stream=fp, title="first_report", description="试一试: first_report")
 runner.run(suite)
+fp.close()
